3dae23602231f198c942068e397e42787bb4321b
rmcassio <cassio_r.m@hotmail.com>
2023-05-22 21:50:29 -0300

    learning about data binding and string interpolation

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'my_first_angular_app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my_first_angular_app');
+    expect(app.name).toEqual('my_first_angular_app');
   });
 
   it('should render title', () => {
